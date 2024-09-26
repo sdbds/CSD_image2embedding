9dfd351ea6f0103f4146a9050100159526cf6f82
sdbds <865105819@qq.com>
2024-09-26 23:51:04 +0800

    fix

--- transfromer2lance.ps1
+++ transfromer2lance.ps1
@@ -3,6 +3,6 @@
 $train_data_dir = "./datasets" # input images path | 图片输入路径
 $output_name = "datasets"
 
-accelerate launch --num_cpu_threads_per_process=8 "./lancedatasets.py" `
+python "./lancedatasets.py" `
   $train_data_dir `
   --output_name=$output_name
